51b34ca8dad78dadba3303e1378e930d329aea77
ppppituch <92442020+VolodymyrTymchyshyn@users.noreply.github.com>
2022-05-27 20:43:07 +0200

    Add files via upload

---
+++ fxmag/components/app/App.js
@@ -0,0 +1,16 @@
+import Contacts from '../contacts/Contacts';
+import MainPart from '../mainPart/MainPart';
+import SocialMedia from '../socialMedia/SocialMedia';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <MainPart />
+      <Contacts />
+      <SocialMedia />
+    </main>
+  );
+}
+
+export default App;
